689a78c93354b391b661417444a7bcc10a52cc4b
useless-stuff <diego.anniballo@gmail.com>
2018-07-22 16:59:43 +0100

    v1.3.3

---
+++ packages/px_functions/src/config/middlewarelist.js
@@ -0,0 +1,15 @@
+const { factory: httpFactory, MIDDLEWARE_NAME: httpName } = require('@serverless-local-proxy/mw_functions_to_http');
+const { factory: httpRequestLoggerFactory, MIDDLEWARE_NAME: httpLoggerName } = require('@serverless-local-proxy/mw_http_request_logger');
+
+const middlewareList = [
+    {
+        name: httpLoggerName,
+        factory: httpRequestLoggerFactory
+    },
+    {
+        name: httpName,
+        factory: httpFactory
+    }
+];
+
+module.exports = { middlewareList };
---
+++ packages/px_functions/src/index.js
@@ -0,0 +1,62 @@
+const EventsManager = require('@serverless-local-proxy/events_manager');
+const Koa = require('koa');
+const Router = require('koa-router');
+const { middlewareList } = require('./config/middlewarelist');
+const { middlewareFactoryGateway } = require('../factorygateway');
+const { factory: stateInjectStoreMiddleware } = require('../../shared/middleware/state/injectstore');
+const { factory: stateInjectEventsManagerMiddleware } = require('../../shared/middleware/state/injecteventmanager');
+const { factory: stateInjectProxyLoggerPrefixMiddleware } = require('../../shared/middleware/state/injectproxyloggerprefix');
+const { factory: storeInjectServiceFunctionsMiddleware } = require('../../shared/middleware/store/injectservicefunctions');
+const LOG_PREFIX = 'FunctionsProxy::';
+
+EventsManager.bind(EventsManager.eventsList.PROXY_START_FUNCTIONS, (config) => functionsProxy(config));
+
+/**
+ * FunctionsProxy
+ *
+ * @constructor
+ * @param proxySettings
+ */
+const functionsProxy = async (proxySettings) => {
+
+    validateProxyConfig(proxySettings);
+
+    const { proxy_host, proxy_port } = proxySettings.config;
+
+    try {
+        const koaServer = new Koa();
+        const koaRouter = new Router();
+
+        const middlewareCollection = middlewareFactoryGateway({
+            middlewareList: middlewareList,
+            proxyConfig: proxySettings.config,
+            serviceFunctions: proxySettings.serviceFunctions,
+            eventsManager: EventsManager,
+            proxyLogPrefix: LOG_PREFIX
+        });
+
+        koaServer.use(stateInjectStoreMiddleware());
+        koaServer.use(stateInjectEventsManagerMiddleware());
+        koaServer.use(stateInjectProxyLoggerPrefixMiddleware(LOG_PREFIX));
+        koaServer.use(storeInjectServiceFunctionsMiddleware(proxySettings.serviceFunctions));
+
+        middlewareCollection.map(middleware => (middleware.factoryType === 'SERVER')
+            ? koaServer.use(middleware.resolver)
+            : koaRouter[middleware.method](middleware.route, middleware.resolver));
+
+        koaServer.use(koaRouter.routes());
+        koaServer.use(koaRouter.allowedMethods());
+        koaServer.listen(proxy_port);
+
+        EventsManager.emitLogInfo(`${LOG_PREFIX} proxy started at ${proxy_host}:${proxy_port}`);
+    } catch (e) {
+        EventsManager.emitLogError(`${LOG_PREFIX} ${e.message}`);
+    }
+
+};
+
+const validateProxyConfig = () => {
+
+};
+
+module.exports = { functionsProxy };
